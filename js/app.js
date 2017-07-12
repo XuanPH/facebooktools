3960b1711704d2648ece9b1d60bd3440e613edc9
Pham Hoang Xuan <phxuan.sy@gmail.com>
2017-07-12 23:42:33 +0700

    nothing

--- js/app.js
+++ js/app.js
@@ -203,12 +203,8 @@ function ($scope, $http, $sce, $log, $routeParams, author) {
     $scope.openNewtab = function (link) {
         window.open(link, '_blank');
     }
-<<<<<<< HEAD
     //remove footer of somee.com
     setTimeout(function () {
         document.getElementsByTagName('center')[0].outerHTML = "";
     }, 500);
-=======
-
->>>>>>> 642ef984d751d77039d685d4dc1255ade06d6da7
 }]);
